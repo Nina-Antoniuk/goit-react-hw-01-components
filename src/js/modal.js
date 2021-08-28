a7f99c445b13cf69d58a41cef02329c40424b671
Nina Antoniuk <Antoniuk_Nina@ukr.net>
2021-08-28 10:33:04 +0300

    cleared a folder

--- src/js/modal.js
+++
@@ -1,7 +0,0 @@
-import * as basicLightbox from 'basiclightbox';
-import largeImage from '../templates/modal.hbs';
-
-
-export function showLargeImage(src) {
-  return  basicLightbox.create(largeImage({largeImageURL: src}))
-}
--- src/js/refs.js
+++
@@ -1,9 +0,0 @@
-const refs = {
-  body: document.body,
-  searchForm: document.querySelector('#search-form'),
-  searchInput: document.querySelector('#search-input'),
-  gallery: document.querySelector('#gallery-js'),
-  loaderBtn: document.querySelector('#loader-js')
-}
-
-export default refs;
